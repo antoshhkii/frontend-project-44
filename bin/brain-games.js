694698194c6385ebda816c6fe6d24fd3de6ec450
antoshhkii <velikii.nord@gmail.com>
2022-11-29 10:30:19 +0300

    Второй этап

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+var readlineSync = require('readline-sync');
+
 const startMsg1 = ('node bin/brain-games.js');
 const startMsg2 = ('Welcome to the Brain Games!');
 console.log(startMsg1);
