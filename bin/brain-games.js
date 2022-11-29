256501d582080673c5d4f94d7cd191952fa5011a
antoshhkii <velikii.nord@gmail.com>
2022-11-29 13:15:57 +0300

    Третий этап

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env 
+import name from "../src/cli.js";
 
-var readlineSync = require('readline-sync');
-
-const startMsg1 = ('node bin/brain-games.js');
-const startMsg2 = ('Welcome to the Brain Games!');
-console.log(startMsg1);
-console.log(startMsg2);
+name()
