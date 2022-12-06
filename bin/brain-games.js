6d7f452a17ec73a3e064acbd21b2fc2c40ca8e72
antoshhkii <velikii.nord@gmail.com>
2022-12-06 14:21:03 +0300

    did insanely stuff

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env
 import name from '../src/cli.js';
-import calc from './brain-calc.js';
-import even from './brain-even.js';
-import gcd from './brain-gcd.js';
-import progression from './brain-progression.js';
 
 name();
-even();
-calc();
-gcd();
-progression();
