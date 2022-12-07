f2437384ffe6b43246d3fd5218e079efa7381f60
Antoshhja <velikii.nord@gmail.com>
2022-12-07 22:28:40 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,10 @@
 import readlineSync from 'readline-sync';
-import { question } from '.';
 
 const greeting = (description) => {
-  question('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  question(`Hello, ${userName}!`);
-  question(description)
+  console.log(`Hello, ${userName}!`);
+  console.log(description)
   return userName;
 };
 
