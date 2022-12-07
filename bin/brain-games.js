85df4ae31f623da36c770fbb67641b1f97a18443
antoshhkii <velikii.nord@gmail.com>
2022-12-07 13:19:43 +0300

    hexlet check

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,14 +1,4 @@
 #!/usr/bin/env
 import greeting from '../src/cli.js';
-import progression from './brain-progression.js';
-import calc from './brain-calc.js';
-import even from './brain-even.js';
-import gcd from './brain-gcd.js';
-import prime from './brain-prime.js';
 
-const name = greeting();
-even(name);
-calc(name);
-gcd(name);
-progression(name);
-prime(name);
+greeting();
